49d01c96c99f7efaebfa716ade7dc2a2bd2851cc
Huijing Liang <47288523+MollyLiang@users.noreply.github.com>
2021-09-08 22:31:00 -0700

    Connection

---
+++ PracticeApp/Antra.PracticeApp.UI.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Antra.PracticeApp.UI.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManageMovie manageMovie = new ManageMovie();
+            manageMovie.Run();
+        }
+    }
+}
